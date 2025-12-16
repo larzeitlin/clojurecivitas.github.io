b5529490f886a05d9727967cbc5af3dd40148cb6
Looke Z <luke@MacBook-Pro.local>
2025-12-16 12:34:04 +0000

    misc edits:
    - add several links, for example to geotiff notebook
    - update wording for `why clojure?`

--- src/gis/roadmap.clj
+++ src/gis/roadmap.clj
@@ -15,13 +15,15 @@
 ;; At present this document can serve as a location to add notes, todos and thoughts.
 ;; Some future version of it may serve as a landing page.
 ;; # Why Clojure?
-;; ## TODO:
-;; A few words about why Clojure is good for GIS...
-;; -  Clojure's ability to interact with multiple host environments (Java, JS, Python, etc).
-;; - Same language on the front and backend
-;; - Ergonomic data workflow
-;; - Interactive development
-;; # GIS Task Categories 
+;; Clojure is, in many ways, ideally suited for GIS work.
+;; - A single language across the stack with access to the Java ecosystem, JS via ClojureScript and Python libraries using [libpython-clj](https://github.com/clj-python/libpython-clj).
+;; - An excellent, interactive namespace-as-a-notebook using [Clay](https://github.com/scicloj/clay) which can break down the boundries between notebooks and production code.
+;; - Exploratory data manipulation is well suited to Clojure's interactive, REPL-driven development style.
+;; - Immutable-by-default, avoiding the pitfalls that can make GIS work (for example in Python) painful.
+
+;; 
+
+;; # Working with GIS data 
 ;; ## Image formats
 ;; ### Raster
 ;; #### Cloud Optimized GeoTIFF (COG)
@@ -42,12 +44,13 @@
 ;; version mismatches between the client language and GDAL.
 ;;
 ;; Some pure language implementations (that do not depend on GDAL) exist, such as
-;; [geotiff.js](https://geotiffjs.github.io/) for JavaScript and [GeoTools](https://geotools.org/) in Java.
+;; [geotiff.js](https://geotiffjs.github.io/) for JavaScript.
 ;; For the sake of
 ;; simplicity we recommend using interop with pure language GeoTIFF library where possible.
 ;;
+;; [Working with GeoTIFFs on the JVM]("/gis/geotiff.html")
+;;
 ;; ##### TODOs:
-;; - GeoTools / Java interop example
 ;; - geotiff.js / JavaScript interop example 
 ;; - Example of a containerized Python bindings with GDAL setup.
 ;;
@@ -60,10 +63,10 @@
 ;; netCDF files in Java-hosted Clojure. In JS we can read NetCDF files with
 ;; [netcdfjs](https://github.com/cheminfo/netcdfjs)
 ;; ##### TODOs:
-;; - GeoTools interop example
+;; - GeoTools/Apache SIS interop example
 ;; - NetCDF-Java example
 ;; - Which one is best?
-;; - netcdfjs example (maybe interactive with scittle/react/clay)
+;; - [netcdfjs](https://github.com/cheminfo/netcdfjs) example (maybe interactive with scittle/react/clay)
 ;;
 ;; #### HDF
 ;; Another widely used multidimensional array format in the scientific GIS world.
@@ -71,6 +74,7 @@
 ;; [HDF in clojure](https://scicloj.github.io/clojure-data-scrapbook/projects/data-formats/hdf/index.html)
 ;; #### Zarr
 ;; ##### TODO:
+;; - review [Python Xarray docs](https://tutorial.xarray.dev/intermediate/intro-to-zarr.html)
 ;; - how to read in Clojure
 ;; 
 ;; ### Vector
@@ -92,7 +96,7 @@
 ;; pount for some of this.
 ;;
 ^:kindly/hide-code
-(kind/image {:src "cogeo-formats-table.png"
+(kind/image {:src "resources/cogeo-formats-table.png"
              :style {:width "30em"}})
 ;; *Image from https:/guide.cloudnativegeo.org* 
 ;;
@@ -114,13 +118,16 @@
 ;; - again, [Factual/geo](https://github.com/factual/geo) may be a good starting point.
 ;;
 ;; ## Map widgets
-;; - Leaflet
-;; - Openlayers
-;; - Kepler.gl
+;; ### Leaflet
+;; ### OpenLayers
+;; [OpenLayers](https://openlayers.org/en/latest/examples/) is a free, open source javascript map widget with wide support for various common data formats including WMS, GeoTIFFs, GeoJSON, etc. It supports webGL layers.
+;; - [Example using OpenLayers from ClojureScript]("/earth_observation/waterquality.html")
+;; ### Kepler.gl
 ;;
 ;; ## Geo-coding / Addresses
 ;;
 ;; ## Remote sensing
+;; Example notebook: [Remote sensing water - Civitas](https://clojurecivitas.github.io/earth_observation/waterquality.html)
 ;; ### Satellite imagery
 ;; - Sentinel2
 ;; - Planet
@@ -140,7 +147,6 @@
 ;; Perhaps we can link to, rework or use some existing work including:
 ;; - [Seattle Parks - Scicloj](https://scicloj.github.io/clojure-data-scrapbook/projects/geography/seattle-parks/index.html)
 ;; - [Chicago Bikes - Scicloj](https://scicloj.github.io/clojure-data-scrapbook/projects/geography/chicago-bikes/index.html)
-;; - [Remote sensing water - Civitas](https://clojurecivitas.github.io/earth_observation/waterquality.html)
 ;; - [Cesium - Civitas](https://clojurecivitas.github.io/cesium/geovis.html)
 ;; - [Clojure Maps Examples](https://github.com/joannecheng/clojure-map-examples)
 ^:kindly/hide-code
